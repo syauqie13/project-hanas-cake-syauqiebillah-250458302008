5b1d95b551d06854a87077824962b9ba7bad1634
syauqie13 <syauqiebillah13@gmail.com>
2026-04-19 14:48:05 +0700

    update model

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
